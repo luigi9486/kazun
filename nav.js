108ac3ad9c1341379620b7afdd16f33eca670433
luigi9486 <ludwik94@gmail.com>
2026-09-24 20:10:53 +0200

    Update navigation header in nav.js

--- nav.js
+++ nav.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // 1. Szablon Nagłówka
     const headerHTML = `
         <nav>
-            <a href="index.html" class="logo">Parafia Kazuń Bielany</a>
+        
             
             <ul class="nav-links">
                 <li><a href="index.html#ogloszenia">Ogłoszenia</a></li>
