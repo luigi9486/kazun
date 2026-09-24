75c17226b6cdbdbd41ed4b7800293c1e7fe9f044
luigi9486 <ludwik94@gmail.com>
2026-09-24 21:36:40 +0200

    Remove logo link from header template

--- nav.js
+++ nav.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // 1. Szablon Nagłówka
     const headerHTML = `
         <nav>
-            <a href="index.html" class="logo">Strona Główna</a>
+            <a href="index.html" class="logo"></a>
             
             <ul class="nav-links">
                 <li><a href="ogloszenia.html">Ogłoszenia</a></li>
